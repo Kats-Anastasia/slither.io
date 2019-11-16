6afecfc02d89884535706c8fcb7987f2cc2fbe80
Kats-Anastasia <Kats.AM@phystech.edu>
2019-11-16 12:19:17 +0300

    Все еще не запускается и не пытается, но везде что-то добавлено

--- Main.py
+++ Main.py
@@ -3,7 +3,6 @@ import math
 import pygame
 from game_object import GameObject
 
-
 class Ball(GameObject):
     def __init__(self, x, y, r, color, speed):
         GameObject.__init__(self, 
@@ -21,22 +20,6 @@ class Ball(GameObject):
                            self.color, 
                            self.center, 
                            self.radius)
-
-class window():
-    def __init__(self):
-        pass
-    def start_game(self, ivent=0)
-        pygame.init()
-        screen = pygame.display.set_mode((800, 600))
-        clock = pygame.time.Clock()
-        background_image = pygame.image.load('images/background2.jpg') # не сработает, так как фотка не ежит там где нужно (А это где?)
-        while True:
-            screen.fill((192, 192, 192))
-            pygame.display.update()
-            clock.tick(60)
-    # Хз как эта штука работает, должна создавать окно с заставкой, где можно ввести имя и выбрать цвет этой гусеницы-змеи-крокодила
-    # Мб в этом классе, мб в другом само окно игровое, на котором генерируется все (уже в игре, не заставка)
-
 
 class ball():
     def __init__(self, x, y, r, color, speed):
@@ -64,24 +47,33 @@ class ball():
         self.x = other.x
         self.y = other.y
         
-    def grow(self, ivent=0)
-        
+    def grow(self, ivent=0):
+        pass
     
-class head(ball):
-    def __init__(self, x, y, r = 10)
+class head(ball): # Мб это будет просто функцией в классе Ball
+    def __init__(self, x, y, r = 10):
+        pass
 
 class meal():
-    def __init__(self, x, y, r=2):
+    def __init__(self, x=0, y=0, r=2):
         self.x = x
         self.y = y
         self.r = r
         self.timer = 0
+        if x == 0 and y == 0:
+            #Создает шарик в случайном месте
+            pass
+        else:
+            #Создает большой шарик на месте сдохшей змеи
+            pass
 
     def pulsation(self):
         # Мерцание
+        pass
 
     def move(self):
         # Медленное движение по кругу
+        pass
 
     def eaten(self, other):
         # Говорит скоко новых шариков создавать (нужно для наследования)
@@ -92,7 +84,7 @@ class big_meal(food):
     def __init__(self, ivent):
         # может быть будет зависеть от чего-то другого, не знаю. В общем если кнопка мыши зажата, он выдет 3 (например)
         # если не зажата, то просто 1. Это коэффециент, на который будет умножаться наша конечная скорость.
-        
+        pass
         # Выглядит как большая еда, которая еще и двигается как-то интересно
         pass
     def move(self):
@@ -100,12 +92,13 @@ class big_meal(food):
         pass
     def something(self):
         # хз что это за штука
+        pass
     def eaten(self, event):
         # Говорит скоко новых шариков создавать. Это число сильно больше чем при обычной еде.
         pass
 
 
-class snake():
+class snake(window):
     def __init__(self, color, name):
         self.color = color
         self.name = name
@@ -114,13 +107,13 @@ class snake():
         
         self.balls = []
         self.an = 0
-        
+        self.timer = 0
         for i in range(50):
-            x1 = 
+            pass
         # name - имя змеи, висящее над ней и которое вводится при запуске, тогда же выбирается и цвет.
         # отвечает за создание начальной змеи начального размера, длины и всего такого. 
 
-    def targeting(self, event = 0)
+    def targeting(self, event = 0): #Нужно объединить со следующей функцией
         if event:
             self.an = math.atan2((event.y-self.y), (event.x-self.x))
         if self.f2_on:
@@ -145,25 +138,60 @@ class snake():
     
     def eat(self, other):
         # Чекает столкнулась ли башка с едой, создает новый шарик, убивает еду, мб что еще делает что я забыла
+        pass
+    
+    def destroyed(self):
+        #Уничтожет змею, на месте каждого шарика создает большую еду
 
+class game():
+    def __init__(self):
+        # Туть рисуется начальный экран, где вводится Имя, выбирается цвет, нажимается кнопка старта игры.        
+        pygame.init()
+        screen = pygame.display.set_mode(1000,1000)
+        start = True
+        self.color = 0
+        self.name = 0
+        while start: # Может лучше не так, а просто чтобы он ждал пока не произойдет событие. Но я не знаю как это сделать
+            for i in pygame.event.get():
+                # if i.type == pygame. # Штука, отвечающая за нажатие кнопки
+                start = false
+                pass
+            # Тут еще хуйня, которая выполняет выбор цвета змеи, вводит название
+            pygame.time.delay(20)
+        self.start_game(name=self.name, color=self.color)
 
-class new_game(): # Делает дохуя всего, прописывать будем ближе к концу, либо наоборот в начале.
-    def __init__():
+    def start_game(self, name, color):
+        screen = pygame.display.set_mode(1000,1000)
+        clock = pygame.time.Clock()
+        self.name = name
+        self.color = color
+        new_snake = snake(name=self.name, color=self.color)
+        snakes = []
+        snakes += new_snake
         self.meals = []
-        
-        Nastya = snake(color='blue', name="Отпочковаться")
-        Masha = snake(color='green', name="Хрючево")
-        Dasha = snake(color='pink', name="Живи")
-        
-        # Допустим, мы создали поле 1000*1000
-        
-        for i in range(500): # создаем кучу еды начальной
-            x1 = random.randrange(10, 990)
-            y1 = random.randrange(10, 990)
-            new_meal = meal(x=x1, y=y1)
-            meals += [new_meal]
-        
-        
+        for i in range (100): # Столько еды создастся изначально
+             new_meal = meal()
+             self.meals += new_meal()
+
+        process = True
+        meal.timer = 0
+        while process:
+            snake.timer += 1 # (ну или сколько там (возможно, таймер не нужен, а он будет только на фишки еды)
+            meal.timer += 1
+            for s in snakes:
+                s.move()
+            if meal.timer >= 5: # Ну или сколько там (Если нужно меньше, можно просто создавать много шариков еды за раз)
+                new_meal = meal()
+                self.meals += new_meal
+            for s in snakes:
+                for l in snakes:
+                    if l != s:
+                        if s.hit(l):
+                            s.destroyed() 
+                            # Для нужной змейки выход из экрана, но возможно здесь вообще надо по-другому писать, надо чекать как работает это все по сети
+            pygame.time.delay(200) #Ну или сколько там нужно времени (между движениями змейки)
+
+new_game = game()
     
     
     
