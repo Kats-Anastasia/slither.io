7984447b7090a617649b3ffac6f037d1ec326e8b
q235-uu <Emelianenko.MA@phystech.edu>
2019-11-21 18:00:10 +0300

    'вспомогательный'

---
+++ config.py
@@ -0,0 +1,8 @@
+import colors #шняга в которой будут задаваться численные параметры и цветовое оформление игры
+
+screen_width = 800
+screen_height = 600
+background_image = 'images/background_1(2).jpg' #потом выберем фон 
+
+
+бла бла бла
