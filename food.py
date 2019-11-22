446ec383129748a3696e656967726c07f533d3eb
Kats-Anastasia <Kats.AM@phystech.edu>
2019-11-23 01:24:10 +0300

    Готов класс еды. Класс большой еды может быть не надо?

--- food.py
+++ food.py
@@ -2,48 +2,67 @@ import pygame
 import colors
 import random
 
+from vector import Vector2d
+import config
+
 class Food():
-    def __init__(self, game, x=0, y=0, DEATH=0, color=0):
+    def __init__(self, game, r=0 x=0, y=0, color=colors.food):
+    '''
+    Создается 1 шарик еды, с заданными координатами, радиусом, цвет выбирается случайно из списка цветов.
+    '''
         self.x = x
         self.y = y
-        self.game = game
-        self.r = 5
-        self.game.energy += 1
-        if self.x == 0 and self.y == 0:       #Создает шарик еды в случайном месте
-            color = random.choice(self.game.COLORS)
-            self.x = random.uniform(5, 9995)  #фиксить параметры окна
-            self.y = random.uniform(5, 9995) 
-            pygame.draw.circle(self.game.screen, color[2], (self.x, self.y), self.r + 4)
-            pygame.draw.circle(self.game.screen, color[0], (self.x, self.y), self.r)
-            pygame.draw.circle(self.game.screen, color[1], (self.x, self.y), self.r - 5)
+        self.coords = Vector2d(self.x, self.y)
+        self.r = r
+        if color == colors.food:
+            self.color = random.choice(color)
         else:
-            if Death !=0:          # Если змея умерла, создает большие шарики еды на месте змеи
-                self.r = 25        # Например
-                pygame.draw.circle(self.game.screen, color[2], (self.x, self.y), self.r + 4)
-                pygame.draw.circle(self.game.screen, color[0], (self.x, self.y), self.r)
-                pygame.draw.circle(self.game.screen, color[1], (self.x, self.y), self.r - 5)
-            else:                  # Если змея оставляет след, генерит шарики еды
-                pygame.draw.circle(self.game.screen, color[2], (self.x, self.y), self.r + 4)
-                pygame.draw.circle(self.game.screen, color[0], (self.x, self.y), self.r)
-                pygame.draw.circle(self.game.screen, color[1], (self.x, self.y), self.r - 5)
-    def eaten(self, other):
-        # Говорит скоко новых шариков создавать (нужно для наследования)
-        pass
+            self.color = color
+        if self.r == 0:
+            self.r = random.uniform(1, 5)
+        self.energy = 0.5 * self.r
+        if self.x == 0 and self.y == 0: # Создает шарик еды в случайном месте
+            self.x = random.uniform(5, 9995)
+            self.y = random.uniform(5, 9995)
+    def draw(self, center, r):
+        '''
+        Рисует шарик еды в случае, если он попадает на экран.
+        Пересчитывает свои "глобальные" координаты в координаты экрана.
+        '''
+        alfa = config.radius / r
+        new_coords = (self.coords - center) * alfa + config.center
+        new_r = self.r * alfa
+        
+        pygame.draw.circle(config.screen, self.color, (new_coords.x, new_coords.y), new_r)
+    
         
         
-class Big_food(Food):
-    def __init__(self, event):
+class Big_food(Food):  # А может быть нахуй???
+    def __init__(self):
         # может быть будет зависеть от чего-то другого, не знаю. В общем если кнопка мыши зажата, он выдет 3 (например)
         # если не зажата, то просто 1. Это коэффециент, на который будет умножаться наша конечная скорость.
-        pass
         # Выглядит как большая еда, которая еще и двигается как-то интересно
         pass
+        
     def move(self):
         # Она там как-то быстро движется, надо подумать как
         pass
+        
     def something(self):
         # хз что это за штука
         pass
+        
     def eaten(self, event):
         # Говорит скоко новых шариков создавать. Это число сильно больше чем при обычной еде.
         pass
+        
+    def draw(self, center, r):
+        '''
+        Рисует шарик еды в случае, если он попадает на экран.
+        Пересчитывает свои "глобальные" координаты в координаты экрана.
+        '''
+        alfa = config.radius / r
+        new_coords = (self.coords - center) * alfa + config.center
+        new_r = self.r * alfa
+        
+        pygame.draw.circle(config.screen, self.color, (new_coords.x, new_coords.y), new_r)
