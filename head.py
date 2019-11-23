20dbbb262e5cc8d5ef31bff7491f5914619dfb52
Kats-Anastasia <Kats.AM@phystech.edu>
2019-11-23 17:28:10 +0300

    Вроде все чтобы работало сделала, но надо будет проверить и подогнать ккоэффициенты

--- head.py
+++ head.py
@@ -1,21 +1,60 @@
 import pygame
 import colors
 import random
-import Ball
 
-class Head(Ball): # Мб это будет просто функцией в классе Ball НЕ БУДЕТ
-    def __init__(self, x, y, color, game, snake):
+from ball import Ball
+import config
+'''
+Не решенные проблемы:
+3) Прорисовка глаз змеи, и их поворот при повороте змеи
+'''
+
+class Head(Ball):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
         self.r = 25
-        self.game = game
-        self.snake = snake
         self.color = color
-        pygame.draw.circle(self.game.screen, self.color[0],(self.x, self.y), self.r)
-        pygame.draw.circle(self.game.screen, self.game.white, (self.x, self, y), self.r) #Бляяяяя а как, тут же надо координаты постоянно пересчитывать как-то...
-    def move(self):
+        self.alfa = 0
+        self.v = 1
+        self.vx = self.v
+        self.vy = 0
+    def move(self, speeding):
         #Обработка мышки
-        self.snake.x = self.x
-        self.snake.y = self.y
-    def hit(self, other): #Пока хз, может и не нужна, но кажется надо
-        pass
+        
+        '''
+        Движение происходит с помощью считывания координат мышки, если
+        угол между скоростью змеи и радиус-вектором до координат мышки
+        больше 90, то он просто заменяется на 90 градусов. Если меньше
+        90, то он проецирует ускорение на ось, перпендикулярную скорости
+        и точно так же прибавляет ускорение. В итоге змея всегда движется
+        по окружности с каким-нибудь радиусом и движение происходит без
+        изломов.
+        '''
+        
+        min_radius = self.r * config.coefficient * speeding
+        acceleration = self.v ** 2 / min_radius
+        
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        mouse_alfa = math.atan2((mouse_y - self.y), (mouse_x - self.x))
+        self_alfa = math.atan2(slef.vy, self.vx)
+        delta_alfa = mouse_alfa - self.alfa
+        
+        if math.fabs(delta_alfa) < math.pi / 2:
+            acceleration *= math.cos(math.pi / 2 - math.copysign(delta_alfa, 1))
+        delta_alfa = math.copysign(pi/2, delta_alfa)
+        mouse_alfa = self_alfa + delta_alfa
+        self.vx += acceleration * math.cos(mouse_alfa)
+        self.vy += acceleration * math.sin(mouse_alfa)
+        self.x += self.vx
+        self.y += self.vy
+        
+    def draw(self, a, b):
+        '''
+        Переменные a и b нужны чтобы не прописывать отдельно
+        движение для головы от остальных шариков. Так они нигде
+        не используются.
+        Голова рисуется все время одинаковой. Потом, возможно, будут добавлены глаза и какая-то такая фигня
+        '''
+        
+        pygame.draw.circle(config.screen, self.color, (config.center.x, config.center.y), config.radius)
