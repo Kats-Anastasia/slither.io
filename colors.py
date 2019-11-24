f0f766f66b92e53e05a270d7b93527d72e1b294c
Kats-Anastasia <Kats.AM@phystech.edu>
2019-11-24 13:26:31 +0300

    new skin

--- colors.py
+++ colors.py
@@ -39,6 +39,16 @@ red_skin_6 = (255, 62, 67)
 red_skin_7 = (255, 51, 56)
 red_skin_8 = (255, 43, 48)
 
+yellow_skin_1 = (252, 254, 140)
+yellow_skin_2 = (250, 254, 110)
+yellow_skin_3 = (249, 254, 76)
+yellow_skin_4 = (248, 254, 43)
+yellow_skin_5 = (248, 253, 11)
+yellow_skin_6 = (235, 241, 1)
+yellow_skin_7 = (233, 239, 1)
+yellow_skin_8 = (225, 231, 1)
+yellow_skin_9 = (219, 224, 1)
+
 bee_skin_1 = (235, 176, 74)
 bee_skin_2 = (70, 70, 70)
 
@@ -50,12 +60,17 @@ red_skin = [red_skin_1, red_skin_2, red_skin_3, red_skin_4,
            red_skin_5, red_skin_6, red_skin_7, red_skin_8,
            red_skin_7, red_skin_6, red_skin_5, red_skin_4,
            red_skin_3, red_skin_2]
+yellow_skin = [yellow_skin_1, yellow_skin_2, yellow_skin_3, yellow_skin_4,
+               yellow_skin_5, yellow_skin_6, yellow_skin_7, yellow_skin_8, 
+               yellow_skin_9, yellow_skin_8, yellow_skin_7, yellow_skin_6,
+               yellow_skin_5, yellow_skin_4, yellow_skin_3, yellow_skin_2]
 bee_skin = [bee_skin_1, bee_skin_2, bee_skin_2]
 
 food = [yellow_food, red_food, green_food, blue_food, pink_food,
         pink_1_food, purple_food, light_blue_food, orange_food] # Массив цветов
 purple_skin = [purple, black, white]
-skins_snake = [bee_skin, red_skin, green_skin] # Массив массивов скинов змей
+skins_snake = [red_skin, green_skin, yellow_skin, bee_skin] # Массив массивов скинов змей
+skins_snake = [red_skin, green_skin, yellow_skin, bee_skin] # Массив массивов скинов змей
 for i in range(2):
     print(i)
 print(purple[1])
