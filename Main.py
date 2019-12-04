eca84c0b9ec4737ccbfa60c5ef9eda47f14cea22
dasha-1 <55547098+dasha-1@users.noreply.github.com>
2019-12-04 17:48:01 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -65,6 +65,24 @@ class Game():
                 s.draw()
             place.draw(new_snake.coords)
             pygame.display.update()
+            
+            
+     def main():
+        run = True
+        n = Network()
+        p = n.getP()
+        clock = pygame.time.Clock()
+
+        while run:
+            clock.tick(60)
+            p2 = n.send(p)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()            
+            
+        
 
 if __name__ == "__main__":
     new_game = Game()
